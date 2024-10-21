660b90c95ea162f547ef52973d2a79cc98fbd712
JEhrhardt99 <jannes.ehrhardt@uni-oldenburg.de>
2024-10-21 11:48:45 +0200

    Setting relative paths

--- main/R_Script/PS_1.R
+++ main/R_Script/PS_1.R
@@ -1,4 +1,8 @@
-# Übung 1
+# Information -------------------------------------------------------------
+
+# Forecasting Methods WiSe 24/25
+# Jannes Ehrhardt
+# Problem Set I
 
 # Commands for version control with git:
 # cd PATH
@@ -8,4 +12,65 @@
 # git push -u origin main
 
 
+# clear global environment
+rm(list = ls())
+
+
+
+
+# Packages ----------------------------------------------------------------
+
+
+# Install
+
+# install.packages("tidyverse")
+
+
+# Load
+
 library(tidyverse)
+library(estimatr)
+library(modelsummary)
+
+
+
+
+# Working Directory -------------------------------------------------------
+
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # Set working directory to script location
+setwd("..") # Move up to the project root directory
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
