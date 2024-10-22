0f86d32c76586be678f9f955d3f0355ecb01f901
JEhrhardt99 <jannes.ehrhardt@uni-oldenburg.de>
2024-10-22 11:28:41 +0200

    Task 1-3

--- main/R_Script/PS_1.R
+++ main/R_Script/PS_1.R
@@ -31,6 +31,8 @@ rm(list = ls())
 library(tidyverse)
 library(estimatr)
 library(modelsummary)
+library(visdat)
+library(dplyr)
 
 
 
@@ -60,11 +62,45 @@ getwd() # check if wd is the root directory
 df_STAN <- read.csv("main/Data_sets/STAN_ALL.csv")
 
 
+vis_dat(df_STAN,
+        warn_large_data = FALSE)
 
 
 
 
 
+# Nr. 2 -------------------------------------------------------------------
+
+
+
+df_small <- df_STAN[c("COUNTRY",
+                      "YEAR",
+                      "VALU",
+                      "WAGE",
+                      "INDUSTRY",
+                      "ISICREV3")]
+
+vis_dat(df_small)
+
+
+
+
+# Nr. 3 -------------------------------------------------------------------
+
+df_small <- df_small[order(df_small$COUNTRY, df_small$INDUSTRY, df_small$YEAR),]
+
+
+### build a group index
+
+df_small$idnr <- df_small %>% group_indices(COUNTRY, INDUSTRY)
+
+### use the group index to get all informations nessacry in one line 
+
+df_small <- df_small %>% group_by(idnr) %>% mutate(VALU_lag_1 = dplyr::lag(VALU, n = 1L, default = NA,))
+
+### calcualte the growth rate of valu added 
+
+df_small$VALU_gr <- (df_small$VALU - df_small$VALU_lag_1)/df_small$VALU_lag_1
 
 
 
