b0bab7ff5f44659dace7f1994f041296c893d676
xoje3080 <jannes.ehrhardt@uni-oldenburg.de>
2024-10-24 10:23:24 +0200

    create EX_1.R

---
+++ main/R_Script/EX_1.R
@@ -0,0 +1,65 @@
+# Information -------------------------------------------------------------
+
+# Forecasting Methods WiSe 24/25
+# Jannes Ehrhardt
+# Exercise I
+
+# Commands for version control with git:
+# cd PATH
+# git status
+# git add .
+# git commit -m "header"
+# git push -u origin main
+
+
+# clear global environment
+rm(list = ls())
+
+
+
+
+# Packages ----------------------------------------------------------------
+
+
+# Install
+
+# install.packages("tidyverse")
+
+
+# Load
+
+library(tidyverse)
+library(estimatr)
+library(modelsummary)
+library(visdat)
+library(dplyr)
+library(ggplot2)
+library(ggthemes)
+library(stringr)
+
+
+
+
+# Working Directory -------------------------------------------------------
+
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # set working directory to script location
+setwd("../..") # move up to the project root directory
+getwd() # check if wd is the root directory
+
+
+
+# Read Data ---------------------------------------------------------------
+
+
+
+# Example for reading data from this repositories root folder:
+
+# data <- read.csv("main/Data_sets/BASF.csv")
+# write.csv(data, "main/Problem_Sets_Output/processed_data.csv")
+
+
+
+
+
+
