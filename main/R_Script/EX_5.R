5fbceb1ba965dc416e076d08a082d0be57ccf948
xoje3080 <jannes.ehrhardt@uni-oldenburg.de>
2024-12-05 12:20:39 +0100

    EX5

---
+++ main/R_Script/EX_5.R
@@ -0,0 +1,95 @@
+# Information -------------------------------------------------------------
+
+# Forecasting Methods WiSe 24/25
+# Jannes Ehrhardt
+# Exercice V
+
+# Commands for version control with git:
+# cd PATH
+# git status
+# git add .
+# git commit -m "header"
+# git push -u origin main
+
+
+# clear global environment
+rm(list = ls())
+
+
+
+# Packages ----------------------------------------------------------------
+
+
+# Install
+
+# install.packages("tidyverse")
+
+
+# Load
+
+library(tidyverse)
+library(estimatr)
+library(modelsummary)
+library(visdat)
+library(dplyr)
+library(ggplot2)
+library(ggthemes)
+library(stringr)
+library(haven)
+library(Hmisc)
+library(expss)
+library(dplyr)
+library(forecast)
+library(lubridate)
+library(zoo)
+library(xts)
+library(gridExtra)
+library(estimatr)
+library(lubridate)
+library(generics)
+library(fpp2)
+
+
+
+# Working Directory -------------------------------------------------------
+
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # set working directory to script location
+setwd("../..") # move up to the project root directory
+getwd() # check if wd is the root directory
+
+
+# Load Data ---------------------------------------------------------------
+
+aus <- austa
+
+autoplot(aus)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
