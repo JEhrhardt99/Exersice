49e546e7fd315ae35d1382366e9a548ec944dba3
xoje3080 <jannes.ehrhardt@uni-oldenburg.de>
2024-12-12 12:58:44 +0100

    set up EX6

---
+++ main/R_Script/EX6.R
@@ -0,0 +1,102 @@
+# Information -------------------------------------------------------------
+
+# Forecasting Methods WiSe 24/25
+# Jannes Ehrhardt
+# Exercice VI
+
+# Commands for version control with git:
+# cd PATH
+# git status
+# git add .
+# git commit -m "header"
+# git push -u origin main
+
+
+# clear global environment
+rm(list = ls())
+
+
+
+# Packages ----------------------------------------------------------------
+
+
+# Install
+
+# install.packages("tidyverse")
+
+
+# Load
+
+library(tidyverse)
+library(estimatr)
+library(modelsummary)
+library(visdat)
+library(dplyr)
+library(ggplot2)
+library(ggthemes)
+library(stringr)
+library(haven)
+library(Hmisc)
+library(expss)
+library(dplyr)
+library(forecast)
+library(lubridate)
+library(zoo)
+library(xts)
+library(gridExtra)
+library(estimatr)
+library(lubridate)
+library(generics)
+library(fpp2)
+library(tseries)
+library(fastDummies)
+
+
+
+# Working Directory -------------------------------------------------------
+
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # set working directory to script location
+setwd("../..") # move up to the project root directory
+getwd() # check if wd is the root directory
+
+
+# Load Data ---------------------------------------------------------------
+
+# load the waren data set
+
+waren <- read_csv("main/Data_sets/waren.csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
