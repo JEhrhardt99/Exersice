def16d99122fe15dc8324990c32a806be2b670e4
xoje3080 <jannes.ehrhardt@uni-oldenburg.de>
2024-12-19 13:40:27 +0100

    EX8

--- main/R_Script/EX_7.R
+++ main/R_Script/EX_7.R
@@ -2,7 +2,7 @@
 
 # Forecasting Methods WiSe 24/25
 # Jannes Ehrhardt
-# Exercice VI
+# Exercice VII
 
 # Commands for version control with git:
 # cd PATH
@@ -40,7 +40,7 @@ library(lubridate)
 library(generics)
 library(fpp3)
 library(tseries)
-library(gridExtra)
+library(cowplot)
 library(mFilter)
 
 
@@ -101,8 +101,126 @@ p2 <- autoplot(log_gdp_ts) +
   theme_stata()
 
 
-# combine both in 1 ggplot next to each other
-grid.arrange(p1, p2, ncol = 2)
+# combine both in 1 ggplot next to each other using cowplot package
+plot_grid(p1, p2, labels = c("A", "B"), ncol = 2)
+
+
+
+# Linear Detrending -------------------------------------------------------
+
+lin_mod <- lm_robust(log_gdp ~ ID,
+                     data = gdp)
+
+
+# extract the fitted values (as ts-object):
+lin_trend <- ts(lin_mod$fitted.values,
+                start = c(1991, 1),
+                frequency = 4)
+
+# cycle component of the trend
+lin_cycle <- log_gdp_ts - lin_trend
+
+
+# plot that
+autoplot(lin_cycle) +
+  theme_minimal() +
+  labs(title = "Linear Detrending",
+       x = "Year",
+       y = "Log GDP") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  theme_stata()
+
+
+# easier way would have been to use the hodrick filter:
+
+
+# Hodrick-Prescott (HP) Filter: -------------------------------------------
+
+# lambda value for cycle = 1600 (quarterly data)
+
+hp_decomp <- hpfilter(log_gdp_ts,
+                      freq = 16000)
+
+# extract information from hp_decomp:
+hp_trend <- hp_decomp$trend
+hp_cycle <- hp_decomp$cycle
+
+# combine these into a data.frame:
+HP_out <- cbind(hp_decomp$x, 
+                hp_decomp$trend,
+                hp_decomp$cycle) 
+
+colnames(HP_out) <- c("x", "trend", "cycle")
+
+# store as df
+HP_out <- as.data.frame(HP_out)
+
+# easier way to plot
+plot(hpfilter(log_gdp_ts, freq = 1600))
+
+# what about the lambda value (freq)
+# what if it is a monthy dataset: 14400
+# annual: 10
+# daily:  14400*30 (check it again)
+
+
+
+# compare a little bit 
+
+# Growth Rates -------------------------------------------------------------------------
+
+gdp <- gdp %>%
+  mutate(
+    gr_log_gdp_1y = log_gdp - lag(log_gdp,
+                                  4),       # lag 4 quarters = lag 1 year
+    gr_log_gdp_1q = log_gdp - lag(log_gdp,
+                                  1)        # lag 1 quarter
+  )
+
+
+# plot 
+
+par(mfrow = c(1,1))
+plot(gdp$gr_log_gdp_1y,
+     type = "l",
+     col = "darkblue",
+     ylab = "Growth Rate",
+     xlab = "Year",
+     main = "Growth Rate of Log GDP (1 year lag)",
+     lwd = 2)
+lines(gdp$gr_log_gdp_1q,
+      type = "l",
+      col = "green",
+      lwd = 2)
+lines(HP_out$cycle,
+      type = "l",
+      col = "red",
+      lwd = 2)
+legend("bottomleft",
+       legend = c("1 year growth", "1 quarter growth", "HP cycle"),
+       col = c("darkblue", "green", "red"),
+       lwd = 2,
+       lty = 1,
+       bty = "n"
+       )
+abline(h = 0,
+       col = "grey")
+
+
+# Question: Explain these plots and explain how the 1 year growth differs from the 1 quarter growth and the HP cycle.
+
+# -> Same fluctuations can be seen. Gr of first quearter shows smaller deviations than one year and is more in line with HP cycle.
+
+# -> HP cycle is combines both fluctuations and is more in between the actual fluctuations of the growth rates. It is a smoother version of the growth rates.
+
+# -> LOOK THAT UP MORE CLOSELY FOR EXAM!
+
+
+
+
+
+
+
 
 
 
---
+++ main/R_Script/EX_8.R
@@ -0,0 +1,264 @@
+# Information -------------------------------------------------------------
+
+# Forecasting Methods WiSe 24/25
+# Jannes Ehrhardt
+# Exercice VIII
+
+# Commands for version control with git:
+# cd PATH
+# git status
+# git add .
+# git commit -m "header"
+# git push -u origin main
+
+
+# clear global environment
+rm(list = ls())
+
+
+
+# Packages ----------------------------------------------------------------
+
+
+# Install
+
+# install.packages("tidyverse")
+
+
+# Load
+
+library(tidyverse)
+library(ggplot2)
+library(ggthemes)
+library(forecast)
+library(lubridate)
+library(lubridate)
+library(fpp3)
+library(tseries)
+library(cowplot)
+library(urca)
+library(readr)
+
+
+
+# Working Directory -------------------------------------------------------
+
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # set working directory to script location
+setwd("../..") # move up to the project root directory
+getwd() # check if wd is the root directory
+
+
+# Load Data ---------------------------------------------------------------
+
+
+
+# Simulate Data: ----------------------------------------------------------
+
+set.seed(1)
+
+# Deterministic Trend -----------------------------------------------------
+
+df <- tibble(
+  t = 1:500,
+  uuid = rnorm(500,       # error term
+              mean = 0,
+              sd = 10)
+) %>%
+  mutate(
+    y = NA_real_,
+    y = case_when(t == 1 ~ 1),
+    dt = case_when(t > 1 ~ 0.9 + t + uuid) # deterministic trend
+  )
+
+# plot 
+ggplot(df, aes(x = t)) +
+  geom_line(aes(y = dt)) +
+  theme_minimal() +
+  labs(title = "Simulated Data",
+       x = "Time",
+       y = "Value") +
+  theme_stata()
+
+
+
+# Random Walk -------------------------------------------------------------
+
+df <- df %>%
+  mutate(
+    rw = cumsum(uuid) # random walk
+  )
+
+# Random Walk with drift -------------------------------------------------------------
+
+# doing it very baic here with base r
+df$st <- df$y
+
+for (i in 2:nrow(df)) {                 # second to nth row
+  df$st[i] <- 0.9 + df$st[i-1] + df$uuid[i]   # 0.9 = constant term (bo) 
+}
+
+
+# Random Walk without Drift -----------------------------------------------
+
+
+# doing it very baic here with base r
+df$rw_nd <- df$y
+
+for (i in 2:nrow(df)) {                 # second to nth row
+  df$rw_nd[i] <- df$rw_nd[i-1] + df$uuid[i]   # 0.9 = constant term (bo) 
+}
+
+
+
+# Plot them togerther -----------------------------------------------------
+
+par(mfrow=c(1,3))
+plot(df$dt,
+     type = "l",
+     col = "darkblue",
+     xlab = "Index",
+     ylab = "Value",
+     main = "Deterministic Trend")
+plot(df$st,
+     type = "l",
+     col = "darkred",
+     xlab = "Index",
+     ylab = "Value",
+     main = "Random Walk with Drift")
+plot(df$rw_nd,
+     type = "l",
+     col = "darkgreen",
+     xlab = "Index",
+     ylab = "Value",
+     main = "Random Walk without Drift")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
